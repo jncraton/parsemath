fd28d814ecca45da4ca98fa775088583773357b8
Jon Craton <jncraton@gmail.com>
2018-09-12 11:38:32 -0400

    Initial commit

---
+++ parsemath/parsemath.py
@@ -0,0 +1,23 @@
+import re
+
+def parsemath(expr):
+  """
+  Parses a mathematical expression and returns a Python object
+
+  Eventually, this should use a proper parser, but for now it is just a series of hacks that convert formal mathematical notation into Python code and then evaluate it.
+
+  Example usage:
+
+  >>> parsemath("[1,2,...,5]")
+  range(1, 6)
+  >>> parsemath("[0,2,...,6]")
+  range(1, 7, 2)
+  """
+
+  m = re.search("\[(\d+),(\d+),...,(\d+)\]", expr)
+
+  if m:
+    new_range = "range(%d,%d,%d)" % (int(m.group(1)), int(m.group(3)) + 1, int(m.group(2)) - int(m.group(1)))
+    expr = expr.replace(m.group(0), new_range)
+  
+  return eval(expr)
