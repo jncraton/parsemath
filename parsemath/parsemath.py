6c8770609b8ebcd081dd01734fc53cf8eb53c447
Jon Craton <jncraton@gmail.com>
2018-09-12 11:56:40 -0400

    Added build status badge

--- parsemath/parsemath.py
+++ parsemath/parsemath.py
@@ -2,6 +2,8 @@
 parsemath
 =========
 
+[![Build Status](https://travis-ci.org/jncraton/parsemath.svg?branch=master)](https://travis-ci.org/jncraton/parsemath)
+
 Parses a mathematical expression and returns a Python object
 
 Eventually, this should use a proper parser, but for now it is just a series of hacks that convert formal mathematical notation into Python code and then evaluate it.
