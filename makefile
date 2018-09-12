19767b9fdee5a829e9a3702e038067de87ee6867
Jon Craton <jncraton@gmail.com>
2018-09-12 11:51:25 -0400

    Generate readme

--- makefile
+++ makefile
@@ -1,2 +1,7 @@
+all: test readme.md
+
 test:
 	python3 -m doctest parsemath/parsemath.py
+
+readme.md: parsemath/parsemath.py
+	python3 -c "from parsemath import parsemath; print(parsemath.__doc__)" > readme.md
--- parsemath/parsemath.py
+++ parsemath/parsemath.py
@@ -1,20 +1,24 @@
+"""
+parsemath
+=========
+
+Parses a mathematical expression and returns a Python object
+
+Eventually, this should use a proper parser, but for now it is just a series of hacks that convert formal mathematical notation into Python code and then evaluate it.
+
+Example usage:
+
+    >>> parsemath("[1,2,...,5]")
+    range(1, 6)
+    >>> parsemath("[0,2,...,6]")
+    range(0, 7, 2)
+    >>> parsemath("Σ[0,2,...,6]")
+    12
+"""
+
 import re
 
 def parsemath(expr):
-  """
-  Parses a mathematical expression and returns a Python object
-
-  Eventually, this should use a proper parser, but for now it is just a series of hacks that convert formal mathematical notation into Python code and then evaluate it.
-
-  Example usage:
-
-  >>> parsemath("[1,2,...,5]")
-  range(1, 6)
-  >>> parsemath("[0,2,...,6]")
-  range(0, 7, 2)
-  >>> parsemath("Σ[0,2,...,6]")
-  12
-  """
 
   for m in re.finditer("\[(\d+),(\d+),...,(\d+)\]", expr):
     new_range = "range(%d,%d,%d)" % (int(m.group(1)), int(m.group(3)) + 1, int(m.group(2)) - int(m.group(1)))
